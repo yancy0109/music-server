7bff8cbfff0401ae11c429b2cbbb065cd7eae040
yyy <1304285474@qq.com>
2022-07-16 15:52:35 +0800

    comment and collect

---
+++ src/main/java/com/server/service/CollectService.java
@@ -0,0 +1,11 @@
+package com.server.service;
+
+
+import com.server.pojo.Collect;
+
+import java.util.List;
+
+public interface CollectService {
+    List<Collect> collectionOfUser(Integer userId);
+    int deleteCollect( Integer userId,  Integer songId);
+}
