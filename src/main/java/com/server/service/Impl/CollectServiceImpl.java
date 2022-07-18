0c27dd7faa36fdad92a95c449e3c63ca3756b017
yancy0109 <1156729237@qq.com>
2022-07-19 01:22:06 +0800

    Song API | Collect API | Comment API

---
+++ src/main/java/com/server/service/Impl/CollectServiceImpl.java
@@ -0,0 +1,41 @@
+package com.server.service.Impl;
+
+import com.server.dao.CollectMapper;
+import com.server.pojo.Collect;
+import com.server.service.CollectService;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
+@Service("collectServiceImpl")
+public class CollectServiceImpl implements CollectService {
+    private CollectMapper collectMapper;
+    @Resource(name="collectMapper")
+    public void setCollectMapper(CollectMapper collectMapper) {
+        this.collectMapper = collectMapper;
+    }
+
+    @Override
+    public List<Collect> collectionOfUser(Integer userId) {
+        return  collectMapper.collectionOfUser(userId);
+    }
+
+    @Override
+    public int deleteCollect(Integer userId, Integer songId) {
+        return collectMapper.deleteCollect(userId,songId);
+    }
+
+    @Override
+    public Boolean isCollection(Collect collect) {
+        System.out.println("============");
+        int res=collectMapper.isCollection(collect);
+
+         return res>0;
+    }
+
+    @Override
+    public int setCollection(Collect collect) {
+        return collectMapper.setCollection(collect);
+    }
+
+}
