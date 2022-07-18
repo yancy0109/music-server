83a8e1e986b478146124ec9e0e9d6f610d11ae0a
lishujie <1579368607@qq.com>
2022-07-18 14:29:19 +0800

    ListSong and SongList

---
+++ src/main/java/com/server/controller/ListSongController.java
@@ -0,0 +1,87 @@
+package com.server.controller;
+
+import com.server.common.ErrorMessage;
+import com.server.common.SuccessMessage;
+import com.server.pojo.ListSong;
+import com.server.service.Impl.ListSongServiceImpl;
+import com.server.service.ListSongService;
+import org.apache.commons.lang3.ObjectUtils.Null;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+
+@Controller
+public class ListSongController {
+
+    @Resource(name="listSongServiceImpl")
+    private ListSongService listSongService;
+
+    // 给歌单添加歌曲 manager
+    @ResponseBody
+    @RequestMapping(value = "/listSong/add", method = RequestMethod.POST)
+    public Object addListSong(HttpServletRequest req) {
+        String song_id = req.getParameter("songId").trim();
+        String song_list_id = req.getParameter("songListId").trim();
+
+        ListSong listsong = new ListSong();
+        listsong.setSongId(Integer.parseInt(song_id));
+        listsong.setSongListId(Integer.parseInt(song_list_id));
+
+        boolean res = listSongService.addListSong(listsong);
+        if (res) {
+            return new SuccessMessage<Null>("添加成功").getMessage();
+        } else {
+            return new ErrorMessage("添加失败").getMessage();
+        }
+    }
+
+    // 删除歌单里的歌曲 manager
+    @RequestMapping(value = "/listSong/delete", method = RequestMethod.GET)
+    public Object deleteListSong(HttpServletRequest req) {
+        String songId = req.getParameter("songId");
+
+        boolean res = listSongService.deleteListSong(Integer.parseInt(songId));
+        if (res) {
+            return new SuccessMessage<Null>("删除成功").getMessage();
+        } else {
+            return new ErrorMessage("删除失败").getMessage();
+        }
+    }
+
+    // 返回歌单里指定歌单 ID 的歌曲 manager  client
+    @RequestMapping(value = "/listSong/detail", method = RequestMethod.GET)
+    public Object listSongOfSongId(HttpServletRequest req) {
+        String songListId = req.getParameter("songListId");
+
+        return new SuccessMessage<List<ListSong>>("添加成功", listSongService.listSongOfSongId(Integer.parseInt(songListId)))
+                .getMessage();
+    }
+
+    // 更新歌单里面的歌曲信息
+    @ResponseBody
+    @RequestMapping(value = "/listSong/update", method = RequestMethod.POST)
+    public Object updateListSongMsg(HttpServletRequest req) {
+        String id = req.getParameter("id").trim();
+        String song_id = req.getParameter("songId").trim();
+        String song_list_id = req.getParameter("songListId").trim();
+
+        ListSong listsong = new ListSong();
+        listsong.setId(Integer.parseInt(id));
+        listsong.setSongId(Integer.parseInt(song_id));
+        listsong.setSongListId(Integer.parseInt(song_list_id));
+
+        boolean res = listSongService.updateListSongMsg(listsong);
+        if (res) {
+            return new SuccessMessage<Null>("修改成功").getMessage();
+        } else {
+            return new ErrorMessage("修改失败").getMessage();
+        }
+    }
+}
---
+++ src/main/java/com/server/service/Impl/ListSongServiceImpl.java
@@ -0,0 +1,49 @@
+package com.server.service.Impl;
+
+import com.server.dao.ListSongMapper;
+import com.server.pojo.ListSong;
+import com.server.service.ListSongService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+//@Service("listSongServiceImpl")
+@Component("listSongServiceImpl")
+public class ListSongServiceImpl implements ListSongService {
+
+    //@Autowired
+    @Resource(name="listSongMapper")
+    private ListSongMapper listSongMapper;
+
+    @Override
+    public List<ListSong> allListSong()
+    {
+        return listSongMapper.allListSong();
+    }
+
+    @Override
+    public boolean updateListSongMsg(ListSong listSong) {
+        return listSongMapper.updateListSongMsg(listSong) > 0;
+    }
+
+    @Override
+    public boolean deleteListSong(Integer songId) {
+        return listSongMapper.deleteListSong(songId) > 0;
+    }
+
+    @Override
+    public boolean addListSong(ListSong listSong)
+    {
+        return listSongMapper.insertSelective(listSong) > 0;
+    }
+
+    @Override
+    public List<ListSong> listSongOfSongId(Integer songListId)
+    {
+        return listSongMapper.listSongOfSongId(songListId);
+    }
+
+}
